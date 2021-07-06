83d951153747699a724c633ff810d26095470775
vivektanpure0 <vivektanpure0@gmail.com>
2021-07-06 15:15:53 +0530

    Create CustomExceptionHandler.java

---
+++ src/main/java/com/cg/spring/boot/demo/exception/CustomExceptionHandler.java
@@ -0,0 +1,17 @@
+package com.cg.spring.boot.demo.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(EmployeeNotFoundException.class)
+	public final ResponseEntity<Object> handleEmployeeNotFoundException() {
+
+		return new ResponseEntity<Object>(null, HttpStatus.NOT_FOUND);
+
+	}
+
+}
