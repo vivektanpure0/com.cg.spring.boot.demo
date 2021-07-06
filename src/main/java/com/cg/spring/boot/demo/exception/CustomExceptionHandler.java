d8d7c6a1cf926112e47f7e1e1d438d6929fdb278
vivektanpure0 <vivektanpure0@gmail.com>
2021-07-06 16:20:40 +0530

    Update CustomExceptionHandler.java

--- src/main/java/com/cg/spring/boot/demo/exception/CustomExceptionHandler.java
+++ src/main/java/com/cg/spring/boot/demo/exception/CustomExceptionHandler.java
@@ -2,9 +2,11 @@ package com.cg.spring.boot.demo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
+@ControllerAdvice
 public class CustomExceptionHandler extends ResponseEntityExceptionHandler {
 
 	@ExceptionHandler(EmployeeNotFoundException.class)
