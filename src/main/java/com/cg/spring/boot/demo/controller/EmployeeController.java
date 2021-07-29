c8fc050fc6d28f6ec7b870502c870b03fe740bb1
vivektanpure0 <vivektanpure0@gmail.com>
2021-07-29 14:48:15 +0530

    Commit
    Commit

--- src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
+++ src/main/java/com/cg/spring/boot/demo/controller/EmployeeController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,13 +21,13 @@ import com.cg.spring.boot.demo.service.EmployeeService;
 
 /**
  * 
- * @author Vaman Deshmukh
  * 
  * REST controller for Employee APIs
  *
  */
 
 @RestController
+@CrossOrigin(origins="*")
 public class EmployeeController {
 
 	private static final Logger LOG = LoggerFactory.getLogger(EmployeeController.class);
@@ -86,7 +87,7 @@ public class EmployeeController {
 		return service.findEmployeeBySalary(salary);
 	}
 
-	@GetMapping("/getall")
+	@GetMapping("/Employee")
 	public List<Employee> getAllEmps() {
 		LOG.info("getAllEmps");
 		return service.getAllEmployees();
